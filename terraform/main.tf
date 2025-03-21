4ef21ac9495bf4bc485fcb10091d061e46707f0f
m.cherkasov <m.cherkasov@geoscan.ru>
2025-03-21 07:39:56 +0300

    test

--- terraform/main.tf
+++ terraform/main.tf
@@ -111,7 +111,7 @@ resource "null_resource" "wait_for_run_init" {
 
   provisioner "local-exec" {
     command = <<-EOF
-      until nc -z ${each.network_interface[0].nat_ip_address} 22; do
+      until nc -z ${each.value.network_interface[0].nat_ip_address} 22; do
           echo "Waiting for VM to be ready..."
           sleep 5
         done
